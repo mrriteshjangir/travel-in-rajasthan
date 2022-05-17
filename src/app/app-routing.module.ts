df4734c3364ad29fc0e345bd1514b34b9b0e57b9
mrriteshjangir <mrriteshjangir@gmail.com>
2022-05-17 18:44:49 +0530

    travel web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "about", component:AboutUsComponent, pathMatch: 'full' },
+  {path : "contact", component:ContactUsComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
